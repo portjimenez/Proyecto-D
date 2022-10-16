e2e2f48d681fdee093916ff62a6afec5d86b0031
nperezs5 <115669649+nperezs5@users.noreply.github.com>
2022-10-16 17:35:41 -0600

    Update proyecto.cpp

--- proyecto.cpp
+++ proyecto.cpp
@@ -8,12 +8,13 @@ struct Diccionario{
 
 };
 void Crear();
+void Leer();
 main ( ) {
-
 	Crear();
+	Leer();
     system ("pause");
-
 }
+
 void Crear(){
 	
 	FILE* archivo = fopen(nombre_archivo,"a+b"); //direccion de guardado de datos
@@ -21,7 +22,7 @@ void Crear(){
 	Diccionario diccionario;
 	do{
 		fflush(stdin);        //codigo de pantalla de los datos a ingresar
-		cout<<"ingrese Palabra:";
+		cout<<"Ingrese Palabra:";
 		cin>>diccionario.palabra;
 		cin.ignore();
 
@@ -40,4 +41,26 @@ void Crear(){
 }while(res=='s'|| res=='S');
 
 fclose(archivo);
+Leer();
 	}
+void Leer (){
+	system ("cls");
+	FILE* archivo = fopen(nombre_archivo,"rb");
+	if (!archivo){
+		archivo = fopen(nombre_archivo,"w+b");
+	}
+	Diccionario diccionario;
+	int id=0; //indice a posicion de resgistro (fila) dentro del archivo
+	fread(&diccionario,sizeof(Diccionario),1,archivo);
+	cout<<"_________________________________________________"<<endl;
+	cout<<" ----------- Contenido del Diccionario ----------"<<endl;
+	do{
+		cout<<"_____________________________"<<endl;
+		cout<<"---------- Palabra ----------"<<endl;
+		cout<<"Traduccion ="<<diccionario.traduccion<<endl;
+		cout<<"Funcion ="<< diccionario.funcion<<endl;
+		fread(&diccionario,sizeof(Diccionario),1,archivo);
+		id+=1;
+	}while(feof(archivo)==0);
+	fclose(archivo);
+}
